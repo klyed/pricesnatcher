2ca0b629d14b8acaadf675e8d4d9b74924299ef5
KLYE <kylejrturnbull@gmail.com>
2017-12-03 16:06:40 -0600

    pricesnatcher.js version 0.0.1
    This is the alpha v0.0.1 of my newest nodeJS script

---
+++ pricesnatcher.js
@@ -0,0 +1,253 @@
+//--------------------------------------------------------------------------
+//---- Pricesnatcher.js v0.0.1 || Open Source Price Feed Node.js Script ----
+//----- Developed by @KLYE || Free to Use for All! || Free to Modify -------
+//---- Rekuirements to run: Node.js + steem.js + fs + prompt + reque-st ----
+//-- TO INSTALL DEPENDENCIES FOR APPLICATION:  npm install request --save --
+//--------- npm install prompt --save + npm install request --save ---------
+//--------------------------------------------------------------------------
+//----- PLEASE DO NOT USE THIS CODE BELOW MALICIOUSLY / FOR EVIL DEEDS -----
+//--------------------------------------------------------------------------
+
+var steem = require('steem');
+var request = require('request');
+var fs = require('fs');
+var prompt = require('prompt');
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+};
+
+var witnessname;
+var wif;
+var url;
+var bkey;
+var interval;
+var voteklye;
+var klye = "klye";
+var votedklye = 0;
+var exchangedata;
+var sbdaskcrude;
+var sbdask;
+var sbdbidcrude;
+var sbdbid;
+var sbdavg;
+var usdaskcrude;
+var usdask;
+var usdbidcrude;
+var usdbid;
+var usdavg;
+var sbdfeedprice;
+
+steem.config.set('websocket', 'wss://gtg.steem.house:8090');
+// steem.config.set('websocket', 'wss://steemd.steemitdev.com');
+// steem.config.set('websocket', 'wss://seed.bitcoiner.me');
+
+console.log("------------------------------------------------------------------");
+console.log("----- Starting Pricesnatcher.js - Witness Price Feed Script ------");
+console.log("----- Developed/Coded By: @KLYE --- BLOG: steemit.com/@klye ------");
+console.log("------------------------------------------------------------------");
+
+if (!fs.existsSync(__dirname + "/pricesnatcher.config")) {
+    console.log("??? NOTICE: No Configuration File Found! Please Run Setup Below!");
+    newconfig();
+} else {
+
+    fs.readFile(__dirname + "/pricesnatcher.config", function(err, details) {
+        if (err) {
+            console.log("!!! ERROR: Unable to Read Configuration File!");
+        }
+        if (details) {
+            var info = JSON.parse(details);
+            console.log("Initializing Price Feed Updater, Loading Config File...");
+            startfeed();
+        };
+    });
+};
+
+function newconfig() {
+    prompt.start();
+
+    prompt.message = "";
+
+    prompt.get([{
+        name: 'witnessname',
+        description: 'Witness Account Name? (No @)',
+        required: true
+    }, {
+        name: 'witnessurl',
+        description: "Witness Campaign URL/Website?",
+        required: true
+    }, {
+        name: 'wifinput',
+        description: "Witness Account Posting Private Key?",
+        required: true,
+        replace: '*',
+        hidden: true
+    }, {
+        name: 'activekey',
+        description: "Witness Account Active Key?",
+        required: true,
+        replace: '*',
+        hidden: true
+    }, {
+        name: 'bkey',
+        description: "Witness Account Block Signing Key?",
+        required: true,
+        replace: '*',
+        hidden: true
+    }, {
+        name: 'interval',
+        description: "Number of Blocks Between Update? (1 block = 3 seconds)",
+        required: false,
+        default: 100
+    }, {
+        name: 'voteklye',
+        description: "Vote KLYE for Witness? (true / false)",
+        required: true,
+        default: true
+    }], function(err, result) {
+
+        if (err) {
+            console.log("!!! ERROR: Something Went Wrong During Config.. Please Restart Service! (ctrl + c to exit)")
+        };
+
+        if (result) {
+
+            var newconfig = {
+                witnessname: result.witnessname,
+                wif: result.wifinput,
+                url: result.witnessurl,
+                activekey: result.activekey,
+                bkey: result.bkey,
+                interval: result.interval,
+                voteklye: result.voteklye
+            };
+
+            console.log("*** SUCCESS: You Completed The Configuration - Saving to Disk!");
+
+            fs.writeFile(__dirname + "/pricesnatcher.config", JSON.stringify(newconfig), function(err, win) {
+                if (err) {
+                    console.log("!!! ERROR: Unable to Save Config to Disk!");
+                };
+                if (win) {
+                    console.log("New Configuration Input Saved");
+
+                    console.log("Initializing Price Feed Updater, Loading Config File...");
+                    startfeed();
+                };
+            }); // END /db/engine/config writeFile
+            startfeed();
+        }; //END if (result)
+    }); // END Setup Prompt
+}; // END newconfig();
+
+function startfeed() {
+
+    fs.readFile(__dirname + "/pricesnatcher.config", function(err, data) {
+        if (err) {
+            console.log("!!! ERROR: Reading Config File!");
+        };
+        if (data) {
+            var confdata = JSON.parse(data);
+
+            witnessname = confdata.witnessname;
+            wif = confdata.wif;
+            url = confdata.url;
+            activekey = confdata.activekey;
+            bkey = confdata.bkey;
+            interval = confdata.interval;
+            voteklye = confdata.voteklye;
+
+            if (votedklye == 0) {
+                if (voteklye == true || voteklye == "true") {
+                    steem.broadcast.accountWitnessVote(activekey, witnessname, klye, true, function(err, result) {
+                        if (err) {
+                            votedklye = 1;
+                            console.log("!!! ERROR: Witness Vote for @KLYE Failed! Duplicate Vote or Bad Keys Config!");
+                            //console.log(err);
+                        }; // END if (err)
+                        if (result) {
+                            votedklye = 1;
+                            console.log("*** SUCCESS: Voted For @KLYE's Witness! Thank you for voting for me!!! You Rock!");
+                            //console.log(result);
+                        }; // END if (result)
+                    }); // END Witness Vote
+
+                } else {
+                    votedklye = 0;
+                    console.log("!!! ERROR: Please consider voting @KLYE as witness to support development and get rid of this message!");
+                }; // END voteklye
+            };
+
+
+        }; // END if (data)
+    }); // End readFile
+
+    request('https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_STEEM&depth=1', function(error, response, body) {
+
+        exchangedata = JSON.parse(body);
+        sbdaskcrude = exchangedata.asks[0];
+        sbdask = sbdaskcrude[0];
+        sbdbidcrude = exchangedata.bids[0];
+        sbdbid = sbdbidcrude[0];
+        sbdavg = ((Number(sbdask) + Number(sbdbid)) / 2).toFixed(8);
+        console.log("STEEM Avg Price: " + sbdavg + " BTC");
+    });
+
+    request('https://poloniex.com/public?command=returnOrderBook&currencyPair=USDT_BTC&depth=1', function(error, response, body) {
+
+        exchangedata = JSON.parse(body);
+        usdaskcrude = exchangedata.asks[0];
+        usdask = usdaskcrude[0];
+        usdask = Number(usdask).toFixed(2);
+        usdbidcrude = exchangedata.bids[0];
+        usdbid = usdbidcrude[0];
+        usdbid = Number(usdbid).toFixed(2);
+        usdavg = ((Number(usdask) + Number(usdbid)) / 2).toFixed(2);
+        console.log("USDT Avg Price: $" + usdavg + " USD/BTC");
+
+        sbdfeedprice = Number(usdavg * sbdavg).toFixed(3);
+
+        if (sbdfeedprice != undefined && sbdfeedprice != NaN && sbdfeedprice != null) {
+            updateprice();
+        } else {
+
+            startfeed();
+        };
+
+    });
+
+    function updateprice() {
+
+        if (sbdfeedprice == NaN) {
+            startfeed();
+        } else {
+            console.log("STEEM Price is Roughly: $" + sbdfeedprice + " USD");
+            var exchangeRate = {
+                "base": sbdfeedprice + " SBD",
+                "quote": "1.000 STEEM"
+            };
+
+            steem.broadcast.feedPublish(activekey, witnessname, exchangeRate, function(err, result) {
+                if (err) {
+                    console.log("!!! ERROR: Price Feed Update FAILED!");
+                    startfeed();
+                };
+                if (result) {
+                    console.log("*** SUCCESS: Price Feed Updated!");
+                    var sleeptime = Number(interval * 3000);
+                    var sleepmins = Number(sleeptime / 60000);
+                    console.log("Next Price Feed Update: " + sleepmins + " Minute(s)!");
+                    var restartfeed = setInterval(startfeed, sleeptime);
+                };
+            });
+
+        };
+
+    };
+};
